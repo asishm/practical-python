fe94c0092cf733ca97d4aa33f4112611f116fab5
asishm <asishkm@gmail.com>
2020-05-30 09:44:09 -0400

    ex: 2.4

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,18 @@
 # report.py
 #
 # Exercise 2.4
+import csv
+
+def read_portfolio(filename):
+    
+    portfolio = []
+
+    with open(filename, 'rt') as f:
+        reader = csv.reader(f)
+        headers = next(reader)
+        for ticker, nshares, share_price in reader:
+            nshares = int(nshares)
+            share_price = float(share_price)
+            portfolio.append((ticker, nshares, share_price))
+    
+    return portfolio
