12f086518ed4cecc619ccfba352fdfb2a3a478e0
asishm <asishkm@gmail.com>
2020-05-30 09:24:11 -0400

    ex: 1.30

--- Work/pcost.py
+++ Work/pcost.py
@@ -2,11 +2,15 @@
 #
 # Exercise 1.27
 
-cost = 0
-with open("./Data/portfolio.csv", "rt") as f:
-    next(f) # skip headers
-    for line in f:
-        ticker, nshares, share_cost = line.split(",")
-        cost += int(nshares) * float(share_cost)
+def portfolio_cost(filename):
+    cost = 0
+    with open(filename, "rt") as f:
+        next(f) # skip headers
+        for line in f:
+            ticker, nshares, share_cost = line.split(",")
+            cost += int(nshares) * float(share_cost)
 
+    return cost
+
+cost = portfolio_cost("Data/portfolio.csv")
 print(f"Total cost: {cost}")
