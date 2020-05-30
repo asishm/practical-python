1f3570968f3f8e047347074ff24495c216b53b9c
asishm <asishkm@gmail.com>
2020-05-30 00:26:44 -0400

    ex: 1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -4,11 +4,14 @@ principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+extra_payment_start_month = 60
+extra_payment_end_month = 108
+extra_payment = 1000
 month = 0
 
 while principal > 0:
-    if month < 12:
-        month_payment = payment + 1000
+    if month >= extra_payment_start_month and month <= extra_payment_end_month:
+        month_payment = payment + extra_payment
     else:
         month_payment = payment
     month = month + 1
